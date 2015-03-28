ecc57389fb13044fb9fdd6e56368c3125c003253
samsung <samsung@192.168.0.102>
2015-03-28 22:22:41 +0100

    Prevent screen from turning off.

--- src/com/tjoris/timekeeper/MainActivity.java
+++ src/com/tjoris/timekeeper/MainActivity.java
@@ -8,11 +8,12 @@ import java.util.Map;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity
 {
@@ -42,6 +43,7 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 		getPlaylist().setOnItemClickListener(new OnItemClickListener()
 		{
